5a7d67ece701c25ce444f62b66d5cd6a52d93236
dimych1978 <dimych1978@gmail.com>
2025-12-20 11:18:38 +0300

    Implementation of cloud use upstash/redis

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -1,10 +1,11 @@
-import { createClient } from '@vercel/kv';
+// lib/kv.ts - для Upstash
+import { Redis } from '@upstash/redis';
 
 if (!process.env.KV_REST_API_URL || !process.env.KV_REST_API_TOKEN) {
-  throw new Error('Missing Vercel KV environment variables');
+  throw new Error('Missing Redis environment variables');
 }
 
-export const kv = createClient({
+export const kv = new Redis({
   url: process.env.KV_REST_API_URL,
   token: process.env.KV_REST_API_TOKEN,
 });
